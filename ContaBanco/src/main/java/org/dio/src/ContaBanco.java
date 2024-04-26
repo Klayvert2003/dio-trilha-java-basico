4431503135e62c799cdd4951876aaa8b8015c498
Klayvert2003 <klayvert.ryan16@gmail.com>
2024-04-26 02:06:24 -0300

    [FEAT]: Adiciona projeto ContaBanco - Santander Bootcamp

--- ContaBanco/src/main/java/org/dio/src/ContaBanco.java
+++ ContaBanco/src/main/java/org/dio/src/ContaBanco.java
@@ -1,25 +1,34 @@
 package org.dio.src;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaBanco {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        while (true){
+            try {
+                Scanner sc = new Scanner(System.in);
 
-        System.out.println("Por favor, digite o número da agência!");
-        int numero = sc.nextInt();
-        sc.nextLine();
+                System.out.println("Por favor, digite o número da agência!");
+                int numero = sc.nextInt();
+                sc.nextLine();
 
-        System.out.println("Por favor, digite qual a sua agência!");
-        String agencia = sc.nextLine();
+                System.out.println("Por favor, digite qual a sua agência!");
+                String agencia = sc.nextLine();
 
-        System.out.println("Por favor, digite seu nome completo!");
-        String nomeCliene = sc.nextLine();
+                System.out.println("Por favor, digite seu nome completo!");
+                String nomeCliene = sc.nextLine();
 
-        System.out.println("Por favor, digite o seu saldo!");
-        double saldo = sc.nextDouble();
-        sc.close();
+                System.out.println("Por favor, digite o seu saldo!");
+                double saldo = sc.nextDouble();
+                sc.close();
 
-        System.out.println("Olá ".concat(nomeCliene).concat(" obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia).concat(", conta ".concat(Integer.toString(numero)).concat(" e seu saldo de ".concat(Double.toString(saldo)).concat(" já está disponível para saque")))));
+                System.out.println("Olá ".concat(nomeCliene).concat(" obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia).concat(", conta ".concat(Integer.toString(numero)).concat(" e seu saldo de ".concat(Double.toString(saldo)).concat(" já está disponível para saque")))));
+
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("O número de agência deve ser um número e o saldo passado desta forma: 1000,20");
+            }
+        }
     }
 }
